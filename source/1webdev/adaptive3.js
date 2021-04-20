945fb846124662357716f625405caad118cc6d47
Alchemist997 <brainbrain160@gmail.com>
2021-04-20 21:01:44 +0300

    Сменил JS на media у WebDev

---
+++ source/1webdev/adaptive3.js
@@ -0,0 +1,77 @@
+let containers = document.querySelectorAll('.container');
+
+let form = document.querySelector('form');
+let name = form.querySelector('input[name=name]');
+let email = form.querySelector('input[name=email]');
+
+let services = document.querySelector('.services .container')
+
+// ===================================================================================
+
+	let adaptive = function () {
+		for (let i = 0; i < containers.length; i++) {
+			containers[i].style.width = '95.2%';
+
+			if (i > 0) { // Условие для пропуска хэдэра
+				containers[i].style.paddingTop = '10%';
+				containers[i].style.paddingBottom = '7.7%';
+			};
+		};
+		console.log('adavtive ON')
+	};
+
+	let static = function () {
+		for (let i = 0; i < containers.length; i++) {
+			containers[i].style.width = '1170px';
+
+			if (i > 0) { // Условие для пропуска хэдэра
+				containers[i].style.paddingTop = '120px';
+				containers[i].style.paddingBottom = '90px';
+			};
+		};
+		console.log('adavtive OFF')
+	};
+
+	let smallWidth = function() {
+		name.placeholder = 'Name';
+		email.placeholder = 'Email';
+		services.style.width = '100%';		
+	};
+
+	let normalWidth = function() {
+		name.placeholder = 'Your NAME';
+		email.placeholder = 'Your Email';
+		services.style.width = '95.2%';		
+	};
+
+// ===================================================================================
+
+	window.addEventListener('load', function() {
+		let width = document.documentElement.clientWidth;
+
+		if (width <= 1260) {
+			adaptive();
+		};
+
+		if (width < 450) {
+			smallWidth();
+		};
+	});
+
+	window.addEventListener('resize', function() {
+		let width = document.documentElement.clientWidth;
+		let inner = window.innerWidth;
+
+		if (width <= 1260) {
+			adaptive();
+		} else {
+			static();
+		};
+
+		if (width < 450) {
+			smallWidth();
+		} else {
+			normalWidth();
+		};
+		console.log(width + ', ' + inner);
+	});
