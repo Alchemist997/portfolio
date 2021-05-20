ff20e6e11c225484ead77e6e35a45e1b5ad41626
Alchemist997 <brainbrain160@gmail.com>
2021-05-21 02:22:44 +0300

    Доработал слайдер 2

--- source/2hello/slider.js
+++ source/2hello/slider.js
@@ -2,6 +2,8 @@ let features = document.querySelector('#features');
 let inputs = features.querySelectorAll('input');
 let labels = features.querySelectorAll('label');
 let items = features.querySelectorAll('figure');
+let prev = features.querySelector('.prev');
+let next = features.querySelector('.next');
 
 for (let i = 0; i < inputs.length; i++) {
 	inputs[i].onchange = function () {
@@ -14,6 +16,22 @@ for (let i = 0; i < inputs.length; i++) {
 				labels[i].style.backgroundColor = 'transparent';
 				labels[i].style.color = '#a6adb0';
 				items[i].style.opacity = 0;
+			};
+		};
+	};
+
+	prev.onclick = function() {
+		for (let i = 0; i < inputs.length; i++) {
+			if (inputs[i].checked && i-1 >= 0) {
+				inputs[i-1].click();
+			};
+		};
+	};
+
+	next.onclick = function() {
+		for (let i = inputs.length-1; i >= 0 ; i--) {
+			if (inputs[i].checked && i+1 < inputs.length) {
+				inputs[i+1].click();
 			};
 		};
 	};
